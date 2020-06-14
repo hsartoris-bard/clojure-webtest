367927cced25e4a02a408d1574f675a83dabe735
Hayden Sartoris <hsartoris@bard.edu>
2020-06-14 17:27:35 +0000

    starting to implement stuff from https://medium.com/swlh/building-a-rest-api-in-clojure-3a1e1ae096e

--- src/webtest/core.clj
+++ src/webtest/core.clj
@@ -1,6 +1,9 @@
 (ns webtest.core
   (:require [org.httpkit.server :as s]
-            [compojure.core :refer [routes POST GET ANY]]))
+            [compojure.core :refer [routes defroutes POST GET ANY]]
+            [ring.middleware.defaults :refer [wrap-defaults api-defaults]]
+            [clojure.pprint :as pp]
+            [clojure.data.json :as json]))
 
 (defonce server (atom nil))
 
@@ -20,6 +23,24 @@
           :headers {"Content-Type" "application/json"}
           :body (format "{\"hello\": \"%s\"}" username)})))
 
+(defn echo-request [req]
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body (->>
+           (pp/pprint req)
+           (str "Request Object: " req))})
+
+(defroutes app-routes
+  (GET "/" [:as req]
+       {:status 200
+        :headers {"Content-Type" "application/json"}
+        :body "{\"hello\": \"world\"}"})
+  (GET "/echo" [] echo-request)
+  (GET "/:username" [username :as req]
+       {:status 200
+        :headers {"Content-Type" "application/json"}
+        :body (format "{\"hello\": \"%s\"}" username)}))
+
 (defn create-server []
   (s/run-server (app) {:port 8080}))
 
@@ -29,4 +50,5 @@
     (reset! server nil)))
 
 (defn -main [& args]
-  (reset! server (s/run-server (app) {:port 8080})))
+  ;(reset! server (s/run-server (app) {:port 8080})))
+  (reset! server (s/run-server (wrap-defaults #'app-routes api-defaults) {:port 8080})))
